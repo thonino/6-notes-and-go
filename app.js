70d4a6fa92896442c6ef2099f88efca6803b706f
thonino <thoninoben@gmail.com>
2024-04-26 00:19:09 +0200

    I added login page and register page, and everything is ok

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 require("dotenv").config();
-const express = require("express");
-
+// express & express-session
+const express = require('express');
+const session = require('express-session');
 const app = express();
+
+// bcrypt
+const bcrypt = require('bcrypt');
+
 
 // Middleware pour parser le JSON
 app.use(express.json());
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Connect to database
 // MongoDB Mongoose et dotenv
@@ -21,6 +29,19 @@ mongoose
     console.error("MongoDB connection error:", err);
   });
 
+
+  app.use(
+    session({
+      secret: "your-secret-key",
+      resave: false,
+      saveUninitialized: true,
+    })
+  );
+
+  // Method-override :
+  const methodOverride = require('method-override');
+  app.use(methodOverride('_method'));
+
 // EJS : 
 app.set('view engine', 'ejs');
 
@@ -30,12 +51,50 @@ app.get("/", (req, res) => {
 });
 
 
-app.post("/quiz", (req, res) => {
-  const { question, options, answer } = req.body;
-  // Logique pour sauvegarder le quiz dans MongoDB
-  res.status(201).send("Quiz créé");
+// GET REGISTER
+app.get('/register', (req, res) => {
+  const user = req.session.user;
+  res.render('RegisterForm', { user: user });
 });
 
+// POST REGISTER
+app.post('/register', function(req, res){
+const userData = new User({
+  name: req.body.name,
+  email: req.body.email,
+  password: bcrypt.hashSync(req.body.password,10),
+  role: req.body.role
+  })
+  userData.save()
+    .then(()=>{ res.redirect('/login')})
+    .catch((err)=>{console.log(err); 
+  });
+});
+
+// GET LOGIN
+app.get('/login', (req, res) => {const user = req.session.user;
+res.render('LoginForm', { user: user });});
+
+// POST LOGIN
+app.post('/login', (req, res) => {
+User.findOne({ name: req.body.name }).then(user => {
+  if (!user) {res.send('Name invalide');}
+  if (!bcrypt.compareSync(req.body.password, user.password)) {
+    res.send('Mot de passe invalide');}
+  req.session.user = user;
+  res.redirect('/');
+})
+.catch(err => console.log(err));
+});
+
+
+// GET LOGOUT
+app.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {console.log(err);} 
+    else {res.redirect('/login');}
+  });
+});
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 // Mongodb et Mongoose :
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
-  pseudo: { type: "String", required: true, unique: true },
+  name: { type: "String", required: true, unique: true },
   email: { type: "String", required: true, unique: true },
   password: { type: "String", required: true },
   role: { type: "String", required: true },
